336a39661e178a1034a18c195dbb88f49c04d242
Ashish Vishwakarma <69335638+ashshvk@users.noreply.github.com>
2025-04-17 21:40:36 +0530

    update and add fish

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,30 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Set environment variables
+set -gx EDITOR nvim
+set -gx VISUAL nvim
+set -gx PATH $HOME/.local/bin $PATH
+
+# Aliases
+alias ls 'eza -l -h -o --git --icons'
+alias ll 'ls -a'
+alias g git
+
+# Starship
+starship init fish | source
+
+# Zoxide
+zoxide init fish | source
+
+# z and fzf
+function zf --description 'Use zoxide + fzf to jump to a directory'
+    zoxide query -l | fzf --height 40% --reverse --prompt="zoxide > " | read -l result
+    if test -n "$result"
+        cd "$result"
+    end
+end
+
+
+set -g fish_greeting ""
