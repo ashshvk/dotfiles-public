97f92128e72767470f68d95893855dbfc450902b
Ashish <69335638+ashshvk@users.noreply.github.com>
2026-04-11 16:20:40 +0530

    update

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,6 +2,13 @@
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_python3_provider = 0
+
+-- netrw settings
+--vim.g.netrw_liststyle = 3
+--vim.g.netrw_banner = 0
+--vim.g.netrw_browse_split = 4
+--vim.g.netrw_altv = 1
+
 require("config.lazy")
 -- Force float transparency (theme sometimes overrides this)
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE", fg = "NONE" })
